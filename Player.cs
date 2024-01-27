23e0146848fa589becc3d8599e0b26da911fa9e7
Ethan Meier <e652@hotmail.ca>
2024-01-26 16:38:40 -0800

    Bike interpolation "works", but I want to look at ways we can fine tune the easing

--- Player.cs
+++ Player.cs
@@ -1,9 +1,9 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody3D
 {
-	private int _maxTurnAngle = 1;
+	[Export(PropertyHint.Range, "0, 50, or_greater")]
+	private int _turnSpeed = 10;
 	private Sprite3D _sprite;
 
 	public override void _Ready()
@@ -13,14 +13,14 @@ public partial class Player : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		// grab vec2 from input
+		// convert to radians
+		// lerp between current position and new position
 		Vector2 direction = Input.GetVector("left", "right", "down", "up");
-
-		float rawAngle = _sprite.Rotation.AngleTo(new Vector3(direction.X, 0, direction.Y));
-
-		GD.Print(rawAngle);
-
-		if (direction == new Vector2()) return;
-
-		_sprite.RotateY(Math.Clamp(rawAngle, -1, 1) * (float)delta);
+		if (direction.X == 0 && direction.Y == 0) { return; }
+		if (_sprite.Rotation.Y == direction.Angle()) { return; }
+		float interpolatedAngle = Mathf.LerpAngle(_sprite.Rotation.Y, direction.Angle(), (float)(_turnSpeed * delta));
+		Vector3 newAngle = new Vector3(0, interpolatedAngle, 0);
+		_sprite.Rotation = newAngle;
 	}
 }
