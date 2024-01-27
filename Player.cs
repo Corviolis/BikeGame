20ff6f4286bcd0444af623de7d6a26e36f7431d5
Ethan Meier <e652@hotmail.ca>
2024-01-27 00:36:37 -0800

    fixed turn radius parented camera to position

--- Player.cs
+++ Player.cs
@@ -3,13 +3,15 @@ using Godot;
 public partial class Player : CharacterBody3D
 {
 	[Export]
-	private float _turnSpeed = 7;	
+	private float _turnSpeed = 7f;	
 	[Export]
-	private float _moveSpeed = 10;
+	private float _maxTurnDelta = Mathf.Pi/6;	
 	[Export]
-	private float _accelerationRate = 0.1f;
+	private float _moveSpeed = 100f;
 	[Export]
-	private float _frictionRate = 0.05f;
+	private float _accelerationRate = 500f;
+	[Export]
+	private float _frictionRate = 5f;
 	private Sprite3D _sprite;
 
 	public override void _Ready()
@@ -19,23 +21,38 @@ public partial class Player : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// turns 180 faster than 90, might need to refactor
-		// for claification, this is because we are moving from point a to point b over a set time,
-		//   rather than changing the angle by a set amount
 		Vector2 direction = Input.GetVector("left", "right", "down", "up");
+		RotateBike(direction, (float)delta);
+		MoveBikeForward(direction, (float)delta);
+	}
+
+	private void RotateBike(Vector2 direction, float delta) {
+		// TODO: add some sort of 'memory', so if you turn 180 it remembers 
+		//   whether your last rotation was clockwise or counter?
 		if (direction != Vector2.Zero) {
-			float interpolatedAngle = Mathf.LerpAngle(Rotation.Y, direction.Angle(), (float)(_turnSpeed * delta));
-			Vector3 newAngle = new Vector3(0, interpolatedAngle, 0);
-			Rotation = newAngle;
+			float clampedDifference = Mathf.Clamp(Mathf.AngleDifference(Rotation.Y, direction.Angle()), -_maxTurnDelta, _maxTurnDelta);
+			float interpolatedAngle = Mathf.LerpAngle(Rotation.Y, Rotation.Y + clampedDifference, _turnSpeed * delta);
+			Vector3 newRotation = new Vector3(0, interpolatedAngle, 0);
+			Rotation = newRotation;
 		}
+	}
+
+	private void MoveBikeForward(Vector2 direction, float delta) {
+		// grab the speed/radius of the current and input velocities
+		// interpolate the speed between the current and input speeds
+		// apply it to the forward direction/angle of the bike's rotation
 
 		if (direction != Vector2.Zero) {
-			// grab the 'length' of the direction, a.k.a. the distance of the joystick from neutral
-			float inputVelocity = Mathf.Sqrt(Mathf.Pow(direction.X, 2) + Mathf.Pow(direction.Y, 2)) * _moveSpeed;
+			// grab the 'length' of the direction, a.k.a. the distance of the joystick from neutral or it's speed
+			float currentSpeed = Mathf.Sqrt(Mathf.Pow(Velocity.X, 2) + Mathf.Pow(Velocity.Y, 2));
+			float inputSpeed = Mathf.Sqrt(Mathf.Pow(direction.X, 2) + Mathf.Pow(direction.Y, 2)) * _moveSpeed;
+			float speed = Mathf.Lerp(currentSpeed, inputSpeed, _accelerationRate * delta);
+
 			// multiply that 'length' by the forward direction of the model
-			Velocity = Velocity.Lerp(new Vector3(Mathf.Cos(Rotation.Y), 0 , -Mathf.Sin(Rotation.Y)) * inputVelocity, _accelerationRate);
+			Velocity = new Vector3(Mathf.Cos(Rotation.Y), 0 , -Mathf.Sin(Rotation.Y)) * speed * delta;
 		} else {
-			Velocity = Velocity.Lerp(Vector3.Zero, _frictionRate);
+			// slow down
+			Velocity = Velocity.Lerp(Vector3.Zero, _frictionRate * delta);
 		}
 
 		MoveAndSlide();
