5544abd0241a758c8fd0c954fd0d9dd297fb8b2f
Ethan Meier <e652@hotmail.ca>
2024-02-01 01:03:02 -0800

    implemented camera zooming

--- characters/you/player/Player.cs
+++ characters/you/player/Player.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace LilBikerBoi.characters.you.player;
 
 public partial class Player : CharacterBody3D
 {
-	public bool MovementActive = true;
+	public bool ridingBike = false;
 
 	[Export]
 	private float _frictionRate = 140f;
@@ -12,24 +14,43 @@ public partial class Player : CharacterBody3D
 	private float _speed = 1300f;
 
 	private AnimatedSprite3D _playerSprite;
+	private Node3D _bike;
+	private Area3D _interactZone;
+	private Sprite3D _interactLabel;
+	private readonly List<Area3D> currentlyOverlappingZones = new();
 
 	public override void _Ready()
 	{
 		_playerSprite = GetNode<AnimatedSprite3D>("AnimatedSprite3D");
+		_bike = GetNode<Node3D>("../Bike");
+		_interactZone = GetNode<Area3D>("InteractZone");
+		_interactLabel = GetNode<Sprite3D>("InteractLabel");
+
+		void incrementAreaEntered(Area3D area) => currentlyOverlappingZones.Add(area);
+		_interactZone.AreaEntered += incrementAreaEntered;
+		void decrementAreaEntered(Area3D area) => currentlyOverlappingZones.Remove(area);
+		_interactZone.AreaExited += decrementAreaEntered;
 	}
-	
+
+	public override void _Process(double delta)
+	{
+		_interactLabel.Visible = currentlyOverlappingZones.Count > 0;
+
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
-		if (!MovementActive) {
+		if (ridingBike) {
+			Position = _bike.Position;
 			return;
 		}
 
 		Vector2 direction = Input.GetVector("left", "right", "down", "up");
-		playerMovement(direction, (float)delta);
+		PlayerMovement(direction, (float)delta);
 		AnimatePlayer(direction);
 	}
 
-	private void playerMovement(Vector2 direction, float delta) {
+	private void PlayerMovement(Vector2 direction, float delta) {
 		if (direction != Vector2.Zero) {
 			Velocity = new Vector3(direction.X * _speed * delta, 0, -direction.Y * _speed * delta);
 		} else {
--- resources/CameraMovement.cs
+++ resources/CameraMovement.cs
@@ -2,23 +2,69 @@ using Godot;
 
 public partial class CameraMovement : Camera3D
 {
+	[Signal]
+	public delegate void ChangeCameraToPlayerEventHandler();
+	[Signal]
+	public delegate void ChangeCameraToBikeEventHandler();
+	[Signal]
+	public delegate void ChangeCameraZoomEventHandler(float distance, float cameraFollowSpeed);
+
+
+	[ExportGroup("Camera Movement")]
 	[Export]
-	private float _cameraFollowSpeed = 1f;
+	private float _cameraFollowSpeedPlayer = 1.2f;
+	[Export]
+	private float _cameraFollowSpeedBike = 0.1f;
 	[Export]
 	private bool _lookAtPlayer = true;
+	[ExportGroup("Camera Distance")]
+	[Export]
+	private float _playerDistance = 10f;
+	[Export]
+	private float _bikeDistance = 17.6f;
+	[Export]
+	private float _cameraChangeSpeed = 1f;
+
 	private Node3D _player;
 	private Node3D _parent;
-	// Called when the node enters the scene tree for the first time.
+	private float _cameraFollowSpeed;
+
 	public override void _Ready()
 	{
 		_player = GetNode<Node3D>("../../Player");
 		_parent = GetParentNode3D();
+
+		ChangeCameraToPlayer += () => ChangeCameraDistance(_playerDistance, _cameraFollowSpeedPlayer);
+		ChangeCameraToBike += () => ChangeCameraDistance(_bikeDistance, _cameraFollowSpeedBike);
+		ChangeCameraZoom += ChangeCameraDistance;
+
+		CameraZoom(_playerDistance, _cameraFollowSpeedPlayer, 0f);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		_parent.Position = _parent.Position.Lerp(_player.Position, _cameraFollowSpeed);
 		if (_lookAtPlayer) LookAt(_player.Position);
 	}
+
+	private void ChangeCameraDistance(float distance, float cameraFollowSpeed) {
+		CameraZoom(distance, cameraFollowSpeed, _cameraChangeSpeed);
+	}
+
+	private void CameraZoom(float distance, float cameraFollowSpeed, float speed) {
+		Vector2 newPosition = new Vector2(Position.Z, Position.Y).Normalized() * distance;
+		
+		Tween cameraZoom = CreateTween();
+		cameraZoom.TweenProperty(this, "position", new Vector3(0, newPosition.Y, newPosition.X), speed)
+			.SetEase(Tween.EaseType.InOut)
+			.FromCurrent()
+			.SetTrans(Tween.TransitionType.Expo);
+		
+		Tween cameraFollow = CreateTween();
+		cameraFollow.TweenProperty(this, "_cameraFollowSpeed", cameraFollowSpeed, speed)
+			.SetEase(Tween.EaseType.InOut)
+			.FromCurrent()
+			.SetTrans(Tween.TransitionType.Expo);
+	}
+
 }
