03c681fd9b1da162a225de53f27a978ed572b973
Ethan Meier <e652@hotmail.ca>
2024-02-02 17:15:03 -0800

    added camera zoom while on bike based on velocity

--- resources/CameraMovement.cs
+++ resources/CameraMovement.cs
@@ -1,4 +1,6 @@
 using Godot;
+using LilBikerBoi.characters.you.bike;
+using LilBikerBoi.characters.you.player;
 
 public partial class CameraMovement : Camera3D
 {
@@ -12,50 +14,55 @@ public partial class CameraMovement : Camera3D
 
 	[ExportGroup("Camera Movement")]
 	[Export]
-	private float _cameraFollowSpeedPlayer = 1.2f;
+	private float _followSpeedPlayer = 1.2f;
 	[Export]
-	private float _cameraFollowSpeedBike = 0.1f;
+	private float _followSpeedBike = 0.1f;
 	[Export]
 	private bool _lookAtPlayer = true;
 	[ExportGroup("Camera Distance")]
 	[Export]
-	private float _playerDistance = 10f;
+	private float _playerZoomDistance = 10f;
 	[Export]
-	private float _bikeDistance = 17.6f;
+	private float _bikeZoomDistance = 17.6f;
 	[Export]
-	private float _cameraChangeSpeed = 1f;
+	private float _zoomChangeSpeed = 1f;
+	[Export]
+	private float _velocityZoomOffset = 1f;
 
-	private Node3D _player;
+	private Player _player;
+	private Bike _bike;
 	private Node3D _parent;
 	private float _cameraFollowSpeed;
+	private float _targetDistance;
 
 	public override void _Ready()
 	{
-		_player = GetNode<Node3D>("../../Player");
+		_player = GetNode<Player>("../../Player");
+		_bike = GetNode<Bike>("../../Bike");
 		_parent = GetParentNode3D();
 
-		ChangeCameraToPlayer += () => ChangeCameraDistance(_playerDistance, _cameraFollowSpeedPlayer);
-		ChangeCameraToBike += () => ChangeCameraDistance(_bikeDistance, _cameraFollowSpeedBike);
+		ChangeCameraToPlayer += () => ChangeCameraDistance(_playerZoomDistance, _followSpeedPlayer);
+		ChangeCameraToBike += () => ChangeCameraDistance(_bikeZoomDistance, _followSpeedBike);
 		ChangeCameraZoom += ChangeCameraDistance;
 
-		CameraZoom(_playerDistance, _cameraFollowSpeedPlayer, 0f);
+		_targetDistance = _playerZoomDistance;
+		CameraTweenZoom(_playerZoomDistance, _followSpeedPlayer, 0);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		_parent.Position = _parent.Position.Lerp(_player.Position, _cameraFollowSpeed);
+		ZoomCameraByVelocity();
 		if (_lookAtPlayer) LookAt(_player.Position);
 	}
 
 	private void ChangeCameraDistance(float distance, float cameraFollowSpeed) {
-		CameraZoom(distance, cameraFollowSpeed, _cameraChangeSpeed);
+		CameraTweenZoom(distance, cameraFollowSpeed, _zoomChangeSpeed);
 	}
 
-	private void CameraZoom(float distance, float cameraFollowSpeed, float speed) {
-		Vector2 newPosition = new Vector2(Position.Z, Position.Y).Normalized() * distance;
-		
+	private void CameraTweenZoom(float distance, float cameraFollowSpeed, float speed) {
 		Tween cameraZoom = CreateTween();
-		cameraZoom.TweenProperty(this, "position", new Vector3(0, newPosition.Y, newPosition.X), speed)
+		cameraZoom.TweenProperty(this, "_targetDistance", distance, speed)
 			.SetEase(Tween.EaseType.Out)
 			.FromCurrent()
 			.SetTrans(Tween.TransitionType.Quart);
@@ -67,4 +74,11 @@ public partial class CameraMovement : Camera3D
 			.SetTrans(Tween.TransitionType.Quart);
 	}
 
+	private void ZoomCameraByVelocity() {
+		Vector2 cameraDirection = new Vector2(Position.Z, Position.Y).Normalized();
+
+		float targetDistance = _targetDistance + (_velocityZoomOffset * (_player.ridingBike ? _bike.Velocity.DistanceTo(Vector3.Zero) : 0f));
+		Vector3 newCameraPosition = new Vector3(0, cameraDirection.Y * targetDistance, cameraDirection.X * targetDistance);
+		Position = Position.Lerp(newCameraPosition, 1f);
+	}
 }
